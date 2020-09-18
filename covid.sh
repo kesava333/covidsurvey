7f3d2f6a9f3aa3091e1481ee28f3411cdd3dc1f5
kesava333 <kesichow333@gmail.com>
2020-09-18 12:16:25 -0400

    Initial Commit

---
+++ covid.sh
@@ -0,0 +1,14 @@
+#./bin/bash
+
+echo "Do You Have Any of these symptoms:
+          Most common symptoms:
+             1. fever
+             2. dry cough
+             3. tirednes"
+read Ans
+if (Ans='yes')
+   echo "Please consult Doctor"
+elif
+    echo "You are in good Hands"
+ fi
+
